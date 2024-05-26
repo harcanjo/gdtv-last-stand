ba1251797a650a63014b0ef3dfeda4c10636e2be
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-05-26 16:50:37 -0300

    feat: added basic player damage

---
+++ Assets/Scripts/PlayerDamage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamage : MonoBehaviour {
+
+	public int damageAmount = 15;
+	public LayerMask enemyLayer;
+
+	void Update ()
+	{
+		Collider[] hits = Physics.OverlapSphere (transform.position, 3f, enemyLayer);
+
+
+		if (hits.Length > 0) {
+			print(hits[0].gameObject.tag);
+			if(hits[0].gameObject.tag == TagsHelper.ENEMY_TAG){
+
+				// print("COLLIDED WITH ENEMY");
+				hits[0].gameObject.GetComponent<EnemyHealth>().ApplyDamage(damageAmount);
+			}
+		}
+
+	}
+}
