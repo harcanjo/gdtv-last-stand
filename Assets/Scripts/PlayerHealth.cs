15da1cb0bf9e7f5fb6f1655ec563b7ca58f7f359
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-05-26 17:32:45 -0300

    feat: added basic player health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int health = 100;
+
+	private PlayerController playerController;
+	private Animator playerAnimator;
+
+	void Awake ()
+	{
+		playerController = GetComponent<PlayerController>();
+		playerAnimator =  GetComponentInChildren<Animator>();
+	}
+
+	public void ApplyDamage(int damageAmount){
+		health -= damageAmount;
+
+		if (health < 0)
+		{
+			health = 0;
+		}
+
+		// TODO: displayHealth value
+
+		if (health == 0)
+		{
+			playerController.enabled = false;
+			playerAnimator.Play(TagsHelper.DEAD_ANIMATION);
+
+			// TODO: call game over
+		}
+	}
+
+
+}
