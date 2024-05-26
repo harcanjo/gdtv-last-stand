f039bb1d9bcb04cc28f181e6c6100cb2d8122e4b
Hugo Arcanjo <hfarcanjo@gmail.com>
2024-05-25 22:24:55 -0300

    chore: player basic flow animations

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	[SerializeField] private float correctionSpeed = 1f; // Adjust as needed
+
+    private void Update()
+    {
+        // Check if the Y position is below 0
+        if (transform.position.y < 0)
+        {
+            // Gradually move the Y position towards 0
+            float newY = Mathf.Lerp(transform.position.y, 0, Time.deltaTime * correctionSpeed);
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        }
+    }
+}
